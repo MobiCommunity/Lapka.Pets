b07ed32731b4d994d0f7926d3b7ea77949ce196d
Dominik <dominikbednarz.ti@gmail.com>
2021-08-13 12:57:38 +0200

    Required added

--- Lapka.Pets.Application/Dto/AddressDto.cs
+++ Lapka.Pets.Application/Dto/AddressDto.cs
@@ -5,9 +5,13 @@ namespace Lapka.Identity.Application.Dto
 {
     public class AddressDto
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public LocationDto GeoLocation { get; set; }
     }
 }
